ec23d91589a7a51ff129945527705c190e3be5be
Inder <isingh30@gmail.com>
2026-01-29 11:50:14 +0530

    Updated: made examples clearer

--- playbook_examples/ClassDemo.java
+++ playbook_examples/ClassDemo.java
@@ -9,9 +9,11 @@
  *   javac ClassDemo.java
  *   java ClassDemo
  */
+// class ClassDemo starts
+
 public class ClassDemo {
 
-    // static member - shared across all instances
+    // static member - shared across all instances because it is at the class level
     public static int instancesCreated = 0;
 
     // private instance field - encapsulated per object
@@ -21,7 +23,7 @@ public class ClassDemo {
     public ClassDemo(String name) {
         this.name = name;
         instancesCreated++;
-        System.out.println("Constructor: created instance '" + name + "'");
+        System.out.println("Constructor: created instance " + name + ", " + instancesCreated + " instances created!");
     }
 
     // protected method (visible to subclasses and package)
@@ -31,13 +33,16 @@ public class ClassDemo {
 
     // public instance method
     public String getName() {
+        //System.out.println("getName called for " + name);
         return name;
     }
 
-    // public static utility method
+    // public static utility method - static is at the class level
     public static void printSummary() {
         System.out.println("Instances created so far: " + instancesCreated);
     }
+
+   // class ClassDemo ends
 
     // A subclass in same file to demonstrate protected visibility and inheritance
     static class DemoChild extends ClassDemo {
@@ -59,7 +64,10 @@ public class ClassDemo {
         ClassDemo object2 = new ClassDemo("beta");
 
         System.out.println("object1 name: " + object1.getName());
+        System.out.println("object2 name: " + object2.getName());
+
         object1.protectedInfo();               // allowed here (same class)
+
         printSummary();                   // static call
         System.out.println("Direct static access: " + ClassDemo.instancesCreated);
 
@@ -73,5 +81,6 @@ public class ClassDemo {
         System.out.println("Final instancesCreated: " + ClassDemo.instancesCreated);
 
         System.out.println("=== ClassDemo complete ===");
+
     }
 }
